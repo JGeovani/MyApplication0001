3b466baa2b3775096fc2569f42032d9ebbddfd4b
JGeovani <geovani.santos74@gmail.com>
2014-10-13 17:59:19 -0300

    Alteração inútil de teste. Nova alteração irrisória!

--- mobile/src/androidTest/java/next/diversao/geovani/com/myapplication0001/ApplicationTest.java
+++ mobile/src/androidTest/java/next/diversao/geovani/com/myapplication0001/ApplicationTest.java
@@ -7,6 +7,7 @@ import android.test.ApplicationTestCase;
  * <a href="http://d.android.com/tools/testing/testing_android.html">Testing Fundamentals</a>
  *
  * Comentário inútil para testar o gerenciamento de versão
+ * Mas uma mudança inútil.
  */
 public class ApplicationTest extends ApplicationTestCase<Application> {
     public ApplicationTest() {
